99e969bcc2e77b773713f46de151c959ca020b99
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2023-10-11 08:46:31 -0600

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
+import { CreateUpdateEmployeesComponent } from './components/create-update-employees/create-update-employees.component';
+import { DetailEmployeesComponent } from './components/detail-employees/detail-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 'path':'listEmployees',component:ListEmployeesComponent },
+  { 'path':'createEmployee',component:CreateUpdateEmployeesComponent},
+  { 'path':'editEmployee/:id',component:CreateUpdateEmployeesComponent},
+  { 'path':'detailsEmployee/:id',component:DetailEmployeesComponent},
+  { 'path':'removeEmployee/:id',component:DetailEmployeesComponent},
+  { 'path':'',redirectTo:'listEmployees',pathMatch:'full'},
+  { 'path':'**',redirectTo:'listEmployees',pathMatch:'full'}
+
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
